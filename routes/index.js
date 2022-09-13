d703a99c09379affbad9f0190073f00f6d2e2e3d
Jordan1211 <gre20057@byui.edu>
2022-09-12 19:08:44 -0600

    added test from videos

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req,res) => {
-    res.send('Harrison Ford Gresham');
+    res.send('Harrison Ford Gresham - plus a little dev test');
 });
 
 module.exports = routes;
