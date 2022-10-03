2688cd089e0a1f3855832a07f435c8bcda451271
Jordan1211 <gre20057@byui.edu>
2022-10-03 13:36:05 -0600

    project final touches

--- controllers/index.js
+++ controllers/index.js
@@ -7,11 +7,7 @@ const { ObjectId } = require('mongodb');
 const Post = require('../models/Post');
 
 const getData = async (req, res, next) => {
-  const result = await mongodb
-    .getDb()
-    .db('contacts')
-    .collection('contacts')
-    .find();
+  const result = await mongodb.getDb().db('contacts').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -30,17 +26,13 @@ const getDataById = async (req, res, next) => {
 
 const createNewContact = async (req, res) => {
   try {
-    const result = await mongodb
-      .getDb()
-      .db('contacts')
-      .collection('contacts')
-      .insertOne({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday,
-      });
+    const result = await mongodb.getDb().db('contacts').collection('contacts').insertOne({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    });
     console.log('The contact was created');
     res.setHeader('Content-Type', 'application/json');
     res.status(201).json(result);
@@ -59,8 +51,8 @@ const updateById = async (req, res, next) => {
         { _id: ObjectId(req.body.id) },
         {
           $set: {
-            favoriteColor: req.body.favoriteColor,
-          },
+            favoriteColor: req.body.favoriteColor
+          }
         }
       );
     console.log('Your update has been successful');
@@ -107,5 +99,5 @@ module.exports = {
   createNewContact,
   updateById,
   deleteById,
-  deleteManyByName,
+  deleteManyByName
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const controller = require('../controllers');
 const router = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/', controller.getData);
 router.get('/:id', controller.getDataById);
