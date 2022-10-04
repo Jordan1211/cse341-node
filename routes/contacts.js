0ce993dddd81bc80f370cccdfa6b2bc1f75f242c
Jordan1211 <gre20057@byui.edu>
2022-10-04 11:41:18 -0600

    reverting ids

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,13 +3,13 @@ const controller = require('../controllers');
 const router = express.Router();
 
 router.get('/', controller.getData);
-router.get('/:_id', controller.getDataById);
+router.get('/:id', controller.getDataById);
 
 router.post('/', controller.createNewContact);
 
-router.put('/:_id', controller.updateById);
+router.put('/:id', controller.updateById);
 
-router.delete('/:_id', controller.deleteById);
+router.delete('/:id', controller.deleteById);
 router.delete('/deleteMany', controller.deleteManyByName);
 
 module.exports = router;
