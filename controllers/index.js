3621f1a275e126120a56bc3a22ac0119ee6f05cc
Jordan1211 <gre20057@byui.edu>
2022-10-04 13:32:49 -0600

    changing new contact output

--- controllers/index.js
+++ controllers/index.js
@@ -35,7 +35,7 @@ const createNewContact = async (req, res) => {
     });
     console.log('The contact was created');
     res.setHeader('Content-Type', 'application/json');
-    res.status(201).json(result);
+    res.status(201).json(result.insertedId);
   } catch (err) {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
