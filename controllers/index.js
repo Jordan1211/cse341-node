400bd7f83266768d65cb1109b08f2be13b52fcbc
Jordan1211 <gre20057@byui.edu>
2022-10-04 15:00:42 -0600

    testing removing duplicate headers

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ const { ObjectId } = require('mongodb');
 const getData = async (req, res) => {
   const result = await mongodb.getDb().db('contacts').collection('contacts').find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
 };
@@ -20,7 +20,7 @@ const getSingle = async (req, res) => {
     .db('contacts')
     .collection('contacts')
     .findOne({ _id: ObjectId(req.params.id) });
-  res.setHeader('Content-Type', 'application/json');
+  // res.setHeader('Content-Type', 'application/json');
   res.status(200).json(result);
 };
 
@@ -34,7 +34,7 @@ const createNewContact = async (req, res) => {
       birthday: req.body.birthday
     });
     console.log('The contact was created');
-    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'application/json');
     res.status(201).json(result);
   } catch (err) {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
@@ -56,7 +56,7 @@ const updateById = async (req, res) => {
         }
       );
     console.log('Your update has been successful');
-    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'application/json');
     res.status(204).json(result);
   } catch (err) {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
@@ -71,7 +71,7 @@ const deleteById = async (req, res) => {
       .collection('contacts')
       .deleteOne({ _id: ObjectId(req.params.id) });
     console.log('The contact was Deleted');
-    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
@@ -85,7 +85,7 @@ const deleteManyByName = async (req, res) => {
       .db('contacts')
       .collection('contacts')
       .deleteMany({ firstName: req.body.firstName });
-    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json(response.error || 'Some error occurred while deleting the many contacts.');
