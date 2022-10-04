1ec30d5abe596ce56090c01a4ee0916aab399fde
Jordan1211 <gre20057@byui.edu>
2022-10-04 14:52:12 -0600

    video updates

--- server.js
+++ server.js
@@ -7,22 +7,20 @@ const indexRoute = require('./routes/index');
 const port = process.env.PORT;
 const app = express();
 const bodyparser = require('body-parser');
-// const mongoose = require('mongoose');
-const cors = require('cors');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 app
   .use(bodyparser.json())
-  .use(cors())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
-    res.setHeader('Access-Control-Allow_methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow_methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
